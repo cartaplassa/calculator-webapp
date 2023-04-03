bcc07eca9de4487340de3a79454a40e64196e108
zarni-ein <filippov@mail.ch>
2023-04-03 20:54:12 +0300

    v0.4b: Simplified calculation function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-const memoryNode = document.querySelector('#memory');
 const resultNode = document.querySelector('#result');
 const numericButtons = document.querySelectorAll('.button-number');
 const operations = document.querySelectorAll('.button-operation');
@@ -6,37 +5,25 @@ const deleteBtn = document.querySelector('#button-del');
 const resetBtn = document.querySelector('#button-reset');
 const equalsBtn = document.querySelector('#button-equal');
 
-const incompleteExpression = /\s[\+\-\x\/]\s$/g;
-const arithmeticExpressionRegex = /\d+(?:\.\d+)?|[\+\-\x\/]/g;
-const operationRegex = /[\+\-\x\/]/;
-const incompleteEquationRegex = /[\+\-\x\/] ?$/;
-const additionRegex = /^-?(\d+(?:\.\d+)?) ?\+ ?(\d+(?:\.\d+)?)/;
-const subtractionRegex = /^-?(\d+(?:\.\d+)?) ?\- ?(\d+(?:\.\d+)?)/;
-const multiplicationRegex = /(\d+(?:\.\d+)?) ?\x ?(\d+(?:\.\d+)?)/;
-const divisionRegex = /(\d+(?:\.\d+)?) ?\/ ?(\d+(?:\.\d+)?)/; //÷
-
 
 class Calculator {
-    constructor(memoryNode, resultNode) {
-        this.memoryNode = memoryNode;
+    constructor(resultNode) {
         this.resultNode = resultNode;
         this.incompleteExpression = /\s[\+\-\*\/]\s$/;
+        this.correctExpression = /^(\d(\.\d+)?)+\s[\+\-\*\/]\s(\d(\.\d+)?)+$/;
+        this.zeroRegex = /(^$)|(^0$)|(\s0$)/;
         this.reset();
     }
 
     reset() {
-        // Memory can exist in 3 states:
-        // 1) Empty string
-        // 2) Ends with number
-        // 3) Ends with operation
-        this.memory = '';
-        this.memoryNode.textContent = '';
+        this.memory = '0';
         this.resultNode.textContent = '0';
     }
 
     adjunctNumber(number) {
         if (number === '.' && this.memory.split(' ').at(-1).includes('.')) return;
-        if (number === '.' && this.incompleteExpression.test(this.memory)) return;
+        if (number === '.' && this.incompleteExpression.test(this.memory)) this.memory += '0';
+        if (!(number === '.') && this.zeroRegex.test(this.memory)) this.memory = this.memory.slice(0, -1);
         this.memory += number;
         this.update();
     }
@@ -44,6 +31,7 @@ class Calculator {
     adjunctOperator(operator) {
         if (this.memory.at(-1) === '.') {this.memory = this.memory.slice(0, -1)};
         if (this.incompleteExpression.test(this.memory)) {this.memory = this.memory.slice(0, -3)};
+        if (this.correctExpression.test(this.memory)) {this.update(true)};
         this.memory += ` ${operator} `;
         this.update();
     }
@@ -55,24 +43,44 @@ class Calculator {
         this.update();
     }
 
-    update() {
-        this.memoryNode.textContent = this.memory;
-        this.resultNode.textContent = this.calculate();
+    update(rewrite = false) {
+        if (this.memory === '') {this.memory = '0'};
+        if (rewrite) {
+            this.memory = this.calculate();
+        }
+        this.resultNode.textContent = this.memory;
     }
 
     calculate(expression = this.memory) {
         if (expression.at(-1) === '.') {expression = expression.slice(0, -1)};
         if (!isNaN(Number(expression))) return Number(expression);
-        if (this.incompleteExpression.test(expression)) return eval(expression.slice(0, -3));
-        return eval(expression)
+        if (this.incompleteExpression.test(expression)) return Number(expression.slice(0, -3));
+        if (this.correctExpression.test(expression)) {
+            const temp = expression.split(' ');
+            let result;
+            switch (temp[1]) {
+                case '+':
+                    result = Number(temp[0]) + Number(temp[2]);
+                    break;
+                case '-':
+                    result = Number(temp[0]) - Number(temp[2]);
+                    break;
+                case '*':
+                    result = Number(temp[0]) * Number(temp[2]);
+                    break;
+                case '/':
+                    result = Number(temp[0]) / Number(temp[2]);
+            }
+            return Number(result.toFixed(6)).toString();
+        }
     }
 }
 
-const calculator = new Calculator(memoryNode, resultNode);
+const calculator = new Calculator(resultNode);
 
 deleteBtn.addEventListener('click', () => calculator.delete());
 resetBtn.addEventListener('click', () => calculator.reset());
-equalsBtn.addEventListener('click', () => calculator.calculate(memoryNode.textContent));
+equalsBtn.addEventListener('click', () => calculator.update(true));
 
 numericButtons.forEach(button => {
   button.addEventListener('click', () => calculator.adjunctNumber(button.value));
